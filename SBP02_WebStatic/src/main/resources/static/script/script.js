c643b5a18a2caacb81c63bb5a7342260141a8172
CooOnl <yts8007@naver.com>
2024-11-20 12:11:08 +0900

    SBP02_WebStatic 정적리소스 사용하기

---
+++ SBP02_WebStatic/src/main/resources/static/script/script.js
@@ -0,0 +1,8 @@
+document.getElementById('goSubMain').addEventListener('click',()=>{
+	
+	location.href='/sub/subMain.html';
+});
+document.getElementById('goMain').addEventListener('click',()=>{
+	
+	location.href='/';
+});
